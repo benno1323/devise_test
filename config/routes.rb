0816c7abc46a1633b8cb12156379736f26dd7bcb
Benito Miranda <benomir82@gmail.com>
2015-08-15 17:29:22 -0500

    Add extra fields to technician sign up view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users
+  devise_for :technicians, controllers: { registrations: 'registrations' }
   resources :ads
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
