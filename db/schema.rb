75ea0b73a4e0e8d60491a388256d7a501d73d445
Benito Miranda <benomir82@gmail.com>
2015-08-15 17:04:16 -0500

    Add extra fields to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815214836) do
+ActiveRecord::Schema.define(version: 20150815220106) do
 
   create_table "ads", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 20150815214836) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.string   "mobile"
+    t.string   "rfc"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
